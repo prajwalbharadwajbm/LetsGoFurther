016252e0a32f63a98520a9db82eecc5f37a1b280
Prajwal Bharadwaj BM <prajwalbm40@gmail.com>
2025-02-19 09:30:29 +0530

    Refactor healthcheck handler to use json.Marshal for JSON encoding instead of fixed-formatted String

--- greenlight/cmd/api/healthcheck.go
+++ greenlight/cmd/api/healthcheck.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 // healthcheckHandler is a HTTP handler
 // returns a 200 OK response and prints message which includes the status, environment and version
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version":%q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	//Create a map which holds the information that we want to send in the response.
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+	js = append(js, '\n')
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("charset", "utf-8")
 
-	w.Write([]byte(js))
+	w.Write(js)
 }
