2f30d969411a609561a0f9f416519722f1f7536f
Prajwal Bharadwaj BM <prajwalbm40@gmail.com>
2025-02-19 09:20:49 +0530

    Update healthcheck handler to return JSON response

--- greenlight/cmd/api/healthcheck.go
+++ greenlight/cmd/api/healthcheck.go
@@ -8,7 +8,11 @@ import (
 // healthcheckHandler is a HTTP handler
 // returns a 200 OK response and prints message which includes the status, environment and version
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	js := `{"status": "available", "environment": %q, "version":%q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("charset", "utf-8")
+
+	w.Write([]byte(js))
 }
