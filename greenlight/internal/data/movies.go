9483ed2de31feb1c752c4cff8c8676afc4da1a40
Prajwal Bharadwaj BM <prajwalbm40@gmail.com>
2025-02-21 11:17:10 +0530

    Add Struct Tags

--- greenlight/internal/data/movies.go
+++ greenlight/internal/data/movies.go
@@ -3,11 +3,11 @@ package data
 import "time"
 
 type Movie struct {
-	ID        int64     // Unique ID for the Movie
-	CreatedAt time.Time // Timestamp when movie was added to the Database
-	Title     string    // Movie Title
-	Year      int32     // Year of Release
-	Runtime   int32     // 	Movie Runtime (in minutes)
-	Genres    []string  // Slice of genres for the movie (romance, comedy, etc)
-	Version   int32     // The version number starts at 1 and will be incremented each time the movie information is updated
+	ID        int64     `json:"id"`         // Unique ID for the Movie
+	CreatedAt time.Time `json:"created_at"` // Timestamp when movie was added to the Database
+	Title     string    `json:"title"`      // Movie Title
+	Year      int32     `json:"year"`       // Year of Release
+	Runtime   int32     `json:"runtime"`    // 	Movie Runtime (in minutes)
+	Genres    []string  `json:"genres"`     // Slice of genres for the movie (romance, comedy, etc)
+	Version   int32     `json:"version"`    // The version number starts at 1 and will be incremented each time the movie information is updated
 }
