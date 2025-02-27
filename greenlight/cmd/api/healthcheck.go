50e6db8d54e26c9f9f1b139d32def0257375cb57
Prajwal Bharadwaj BM <prajwalbm40@gmail.com>
2025-02-27 12:02:15 +0530

    Simplify healthcheck error handling

--- greenlight/cmd/api/healthcheck.go
+++ greenlight/cmd/api/healthcheck.go
@@ -22,8 +22,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
-		app.logger.Println(err)
 		app.serverErrorResponse(w, r, err)
-		return
 	}
 }
