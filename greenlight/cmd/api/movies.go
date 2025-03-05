408577160690cfa2a09d86f491c67272b5f6db05
Prajwal Bharadwaj BM <prajwalbm40@gmail.com>
2025-03-05 09:57:31 +0530

    add Validate Movie Function to validate input from client

--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/prajwalbharadwajbm/LetsGoFurther/greenlight/internal/data"
+	"github.com/prajwalbharadwajbm/LetsGoFurther/greenlight/internal/validator"
 )
 
 // @Summary Create a new movie
@@ -28,6 +29,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	// Initialize a new Validator
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
