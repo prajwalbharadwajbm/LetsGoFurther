acf0b8c4b546c4f611e16347acc98af534fa6a0d
Prajwal Bharadwaj BM <prajwalbm40@gmail.com>
2025-03-14 10:09:40 +0530

    update showMovieHandler to adhere to get method

--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/prajwalbharadwajbm/LetsGoFurther/greenlight/internal/data"
 	"github.com/prajwalbharadwajbm/LetsGoFurther/greenlight/internal/validator"
@@ -74,16 +74,16 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "casablanca",
-		Year:      2001,
-		Runtime:   145,
-		Genres:    []string{"comedy", "romance"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
-
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
